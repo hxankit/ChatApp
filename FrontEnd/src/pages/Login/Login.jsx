ee2774acc39a53a44fc4749c36dfb5d53eba2b8d
Mr Ankit <hxankit01@gmail.com>
2024-10-07 23:11:54 +0530

    Backend Completed expect Socket.io And Moving on Frontend

---
+++ FrontEnd/src/pages/Login/Login.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Login() {
+  
+   
+    return (
+        <div className='p-4 h-screen flex items-center justify-center'>
+            <div className='p-6 w-full max-w-md rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+                <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                    Login <span className='text-blue-500'>ChatApp</span>
+                </h1>
+                <form>
+                    <div>
+                        <label className='label p-2'>
+                            <span className='text-base label-text'>Username</span>
+                        </label>
+                        <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+                    </div>
+
+                    <div>
+                        <label className='label'>
+                            <span className='text-base label-text'>Password</span>
+                        </label>
+                        <input
+                            type='password'
+                            placeholder='Enter Password'
+                            className='w-full input input-bordered h-10'
+                        />
+                    </div>
+                    <a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+                        {"Don't"} have an account?
+                    </a>
+
+                    <div>
+                        <button className='btn btn-block btn-sm mt-2'>Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+  
+}
+
+export default Login
